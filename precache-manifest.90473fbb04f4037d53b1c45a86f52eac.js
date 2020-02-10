09f1b97f8331c1e999b3b8e5fe5f021958fa3a8f
Serg <bloodfast13@gmail.com>
2020-02-10 15:16:51 +0200

    Updates

---
+++ precache-manifest.90473fbb04f4037d53b1c45a86f52eac.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06bfcda3e693142d06731b51a6c77cdb",
+    "url": "/HWreactAPP1/index.html"
+  },
+  {
+    "revision": "7c2b28d013b692edf776",
+    "url": "/HWreactAPP1/static/css/main.ed84e775.chunk.css"
+  },
+  {
+    "revision": "ac78ec64a57cdf1d5f49",
+    "url": "/HWreactAPP1/static/js/2.12d055fd.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/HWreactAPP1/static/js/2.12d055fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c2b28d013b692edf776",
+    "url": "/HWreactAPP1/static/js/main.0fd112c6.chunk.js"
+  },
+  {
+    "revision": "e390ece2440396723aca",
+    "url": "/HWreactAPP1/static/js/runtime-main.1bf8d059.js"
+  },
+  {
+    "revision": "e45d9d1f63b5b15b9ebf933e7d9088d3",
+    "url": "/HWreactAPP1/static/media/252.e45d9d1f.png"
+  },
+  {
+    "revision": "76d344f08412543c74ccd1fb2493d607",
+    "url": "/HWreactAPP1/static/media/github-logo.76d344f0.svg"
+  }
+]);
